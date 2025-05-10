f77e68bcf7b4a3756bbe9f7f64e1a93a908bc485
Tarzan-0129 <tajandev0129@gmail.com>
2025-05-10 17:44:24 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 4173,  // Explicitly set preview port
+    strictPort: true, // Prevent automatic port switching
+  }
+})
